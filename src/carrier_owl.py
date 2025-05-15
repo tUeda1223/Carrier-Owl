0201b93ad657345c3f0a96f8f939677adfb317b6
Tetsuya Ueda <107601918+tUeda1223@users.noreply.github.com>
2025-05-16 00:15:41 +0900

    Update carrier_owl.py
    deepLを嚙ませなくした

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -117,31 +117,33 @@ def get_translated_text(from_lang: str, to_lang: str, from_text: str, driver) ->
     '''
     https://qiita.com/fujino-fpu/items/e94d4ff9e7a5784b2987
     '''
-
-    sleep_time = 1
-
-    # urlencode
-    from_text = urllib.parse.quote(from_text)
-
-    # url作成
-    url = 'https://www.deepl.com/translator#' \
-        + from_lang + '/' + to_lang + '/' + from_text
-
-    driver.get(url)
-    driver.implicitly_wait(10)  # 見つからないときは、10秒まで待つ
-
-    for i in range(30):
-        # 指定時間待つ
-        time.sleep(sleep_time)
-        html = driver.page_source
-        # to_text = get_text_from_page_source(html)
-        to_text = get_text_from_driver(driver)
-
-        if to_text:
-            break
-    if to_text is None:
-        return urllib.parse.unquote(from_text)
-    return to_text
+    if False:
+        sleep_time = 1
+    
+        # urlencode
+        from_text = urllib.parse.quote(from_text)
+    
+        # url作成
+        url = 'https://www.deepl.com/translator#' \
+            + from_lang + '/' + to_lang + '/' + from_text
+    
+        driver.get(url)
+        driver.implicitly_wait(10)  # 見つからないときは、10秒まで待つ
+    
+        for i in range(30):
+            # 指定時間待つ
+            time.sleep(sleep_time)
+            html = driver.page_source
+            # to_text = get_text_from_page_source(html)
+            to_text = get_text_from_driver(driver)
+    
+            if to_text:
+                break
+        if to_text is None:
+            return urllib.parse.unquote(from_text)
+        return to_text
+    else:
+        return to_text
 
 def get_text_from_driver(driver) -> str:
     try:
